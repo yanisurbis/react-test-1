1edca6ec9cc06650228188cd29b9ab32f03c732d
Yanis <yanis.urbis@gmail.com>
2016-09-15 18:37:06 +0300

    refactor app to stateless

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-import TimeTracker from './time_tracker'
+import TimeTracker from '../containers/time_tracker'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="flex-center">
-        <div className="container">
-          <TimeTracker />
-        </div>
+const App = () => {
+  return (
+    <div className="flex-center">
+      <div className="container">
+        <TimeTracker />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
